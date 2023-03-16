d550fe8d8a31a9f6a5913281723f5b271fc2b958
Ahmet Mujanovic <mujanovic3737@gmail.com>
2023-03-16 19:42:20 -0400

    Changed static HTML data to JSON for Travel.hbs

--- app_server/controller/travel.js
+++ app_server/controller/travel.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 // When request comes in, it will be assigned to this variable.
 const travel = (req, res) => {
-    res.render('travel', {title: "Travlr Getaways - Travel"});     // Controller will render travel view
+    res.render('travel', {title: "Travlr Getaways - Travel", trips });     // Controller will render travel view
 };
 
 // Export the module
