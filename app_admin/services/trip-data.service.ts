86e62d2336d6195ed3077d8b652f3545b60213af
Ahmet Mujanovic <mujanovic3737@gmail.com>
2023-04-08 07:34:36 -0400

    Add functionality to add and edit trips

--- app_admin/services/trip-data.service.ts
+++ app_admin/services/trip-data.service.ts
@@ -20,10 +20,28 @@ export class TripDataService {
       .catch(this.handleError);
   }
 
+  public getTrip(tripCode: string): Promise<Trip> {
+    console.log('Inside TripDataService#getdTrip(tripCode)');
+    return this.http
+      .get(this.tripUrl + tripCode)
+      .toPromise()
+      .then(response => response.json() as Trip)
+      .catch(this.handleError);
+  }
+
   public getTrips(): Promise<Trip[]> {
     console.log('Inside TripDataService#getTrips');
     return this.http
-      .get(`${this.apiBaseUrl}trips`)
+      .get(this.tripUrl)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+  }
+
+  public updateTrip(formData: Trip): Promise<Trip> {
+    console.log('Inside TripDataService#updateTrip');
+    return this.http
+      .put(this.tripUrl + formData.code, formData)
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
